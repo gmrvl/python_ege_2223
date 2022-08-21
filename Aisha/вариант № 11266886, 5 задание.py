060c2a86af7daff0b35b8a52b76d7c49eefe6f11
AishaYarul <gaisha.yarul@gmail.com>
2022-08-21 22:43:16 +0300

    5 номер

---
+++ Aisha/вариант № 11266886, 5 задание.py
@@ -0,0 +1,11 @@
+for i in range (10000):
+    s = i
+    s = s // 7
+    n = 1
+    while s < 255:
+        if (s+n) % 2 == 0:
+             s = s + 11
+        n = n + 5
+    if n == 96:
+            print(i)
+            break
