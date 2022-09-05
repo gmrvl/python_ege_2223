3ea1347853815591d2674ed59acd8a3a7f293aae
AishaYarul <gaisha.yarul@gmail.com>
2022-09-05 19:32:38 +0300

    вариант № 11286451

---
+++ Aisha/11286451/11 номер.py
@@ -0,0 +1,9 @@
+for i in range(1, 10001):
+    s = i
+    s = 3 * (s // 10)
+    n = 1
+    while s < 221:
+        s = s + 13
+        n = n * 2
+    if n == 128:
+        print(i)
---
+++ Aisha/11286451/15 номер.py
@@ -0,0 +1,11 @@
+for i in range(1, 10001):
+    x = i
+    l = 0
+    m = 1
+    while x > 0:
+        l += 1
+        if x % 2 > 0:
+            m *= x % 8
+        x = x // 8
+    if m == 25 and l == 3:
+        print(i)
