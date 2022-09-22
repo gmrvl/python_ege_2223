562727d1105cdf0af073f874958f9aa7d31081a0
Liika Gumerova <gmrvl@mail.ru>
2022-09-22 08:11:34 +0300

    a - работа над ошибками

--- Aisha/11373493/3.14.py
+++ Aisha/11373493/3.14.py
@@ -1,6 +1,6 @@
 s = 343 ** 6 - 7 ** 10 + 47
 
-count = 6
+count = 0
 while s != 0:
     if s % 7 == 6:
         count += 1
--- Aisha/11373493/6.14.py
+++ Aisha/11373493/6.14.py
@@ -1,5 +1,5 @@
 s = 9 ** 8 + 3 ** 8 - 2
-count = 2
+count = 0
 while s != 0:
     if s % 3 == 2:
         count += 1
