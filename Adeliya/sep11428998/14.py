ee9476e88a6b3082e5fd579384a8a194dacd6210
gmrvl <4qZfpPyJ8Ub2Van>
2022-10-04 08:24:47 +0300

    g - 14 заданиe

--- Adeliya/sep11428998/14.py
+++ Adeliya/sep11428998/14.py
@@ -1,6 +1,13 @@
-a =343**5 + 343**4 + 49**6 - 7**13 - 21
+a = 343 ** 5 + 343 ** 4 + 49 ** 6 - 7 ** 13 - 21
 # 3435 + 3434 + 496 − 713 − 21
-while a!=0:
-    if a%7==0:
-     a//=7
-print(a)
+b = []
+for i in range(7):
+    b.append(0)
+while a != 0:
+    b[a % 7] += 1
+    a //= 7
+count = 0
+for i in b:
+    if i != 0:
+        count += 1
+print(count)
