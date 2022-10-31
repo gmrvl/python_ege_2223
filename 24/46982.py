1c8874f34526e911ea3de981dc13275b0eb59a86
gmrvl <4qZfpPyJ8Ub2Van>
2022-10-31 18:59:12 +0300

    g - разные задачи

---
+++ 24/46982.py
@@ -0,0 +1,16 @@
+file = open('46982.txt').read()
+
+group_count = 0
+count = 0
+for i in file:
+    if i == 'E':
+        count += 1
+        if count >= 12:
+            group_count += 1
+        count = 1
+    elif i == 'F':
+        count = 0
+    else:
+        if count > 0:
+            count += 1
+print(group_count)
