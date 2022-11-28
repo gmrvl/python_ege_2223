69e552267de12524242f553bd46aa8129d3a5b2a
gmrvl <4qZfpPyJ8Ub2Van>
2022-11-28 19:00:27 +0300

    g - 17 и 25

--- Adeliya/11825943/17.1.py
+++ Adeliya/11825943/17.1.py
@@ -1,17 +1,16 @@
-file=open('17.1.txt')
-count=0
-last=0
-summ=0
-maxsumm=0
+file = open('17.1.txt')
+count = 0
+last = int(file.readline())
+summ = 0
+maxsumm = 0
 for i in file:
-    i=int(i)
-    if last%3==0 or i%3==0:
-        count+=1
-        summ=last+i
+    i = int(i)
+    if last % 3 == 0 or i % 3 == 0:
+        count += 1
+        summ = last + i
         if summ > maxsumm:
-            maxsumm=summ
-    last=i
-if summ>maxsumm:
-    maxsumm=summ
-print(count,maxsumm)
-
+            maxsumm = summ
+    last = i
+if summ > maxsumm:
+    maxsumm = summ
+print(count, maxsumm)
--- Adeliya/11867361/17.py
+++ Adeliya/11867361/17.py
@@ -4,50 +4,47 @@
 #  затем максимальную из сумм элементов таких пар.
 #  В данной задаче под парой подразумевается два различных элемента последовательности.
 #  Порядок элементов в паре не важен.
-file=open('17.txt')
-count=0
-ch=0
-max_ch=0
-nch=0
-max_nch=0
-ch19=0
-maxch19=0
-max2ch19=0
-nch19=0
-maxnch19=0
-max2nch19=0
+file = open('17.txt')
+count = 0
+ch = 0
+max_ch = 0
+nch = 0
+max_nch = 0
+ch19 = 0
+maxch19 = 0
+max2ch19 = 0
+nch19 = 0
+maxnch19 = 0
+max2nch19 = 0
 for i in file:
-    i=int(i)
-    if i%2==0 and i%19==0:
-        ch+=1
-        ch19+=1
-        count+=ch
-        if i>maxch19:
-            maxch19=i
-        elif i>max2ch19:
-            max2ch19=i
-    if i%2!=0 and i%19==0:
-        nch+=1
-        nch19+=1
-        count+=nch
-        if i> maxnch19:
-            maxnch19=i
-        elif i> max2nch19:
-            max2nch19=i
-    if i%2==0 and i%19!=0:
-        count+= ch19
-        ch+=1
-        if i> max_ch:
-            max_ch=i
-    if i%2!=0 and i%19!=0:
-        count+= nch19
-        nch+=1
-        if i>max_nch:
-            max_nch=i
-maxsumm=max(max_ch+maxch19,max_nch+maxnch19,maxch19+max2ch19,maxnch19+max2nch19)
-print(count,maxsumm)
-
-
-
-
-
+    i = int(i)
+    if i % 2 == 0 and i % 19 == 0:
+        count += ch
+        ch += 1
+        ch19 += 1
+        if i > maxch19:
+            max2ch19 = maxch19
+            maxch19 = i
+        elif i > max2ch19:
+            max2ch19 = i
+    if i % 2 != 0 and i % 19 == 0:
+        count += nch
+        nch += 1
+        nch19 += 1
+        if i > maxnch19:
+            max2nch19 = maxnch19
+            maxnch19 = i
+        elif i > max2nch19:
+            max2nch19 = i
+    if i % 2 == 0 and i % 19 != 0:
+        count += ch19
+        ch += 1
+        if i > max_ch:
+            max_ch = i
+    if i % 2 != 0 and i % 19 != 0:
+        count += nch19
+        nch += 1
+        if i > max_nch:
+            max_nch = i
+maxsumm = max(max_ch + maxch19, max_nch + maxnch19, maxch19 + max2ch19, maxnch19 + max2nch19)
+print(count, maxsumm)
