dec904f8331f2c4a65bbd6329ab456600c3b27ff
gmrvl <4qZfpPyJ8Ub2Van>
2022-12-01 11:23:48 +0300

    а - задания 5 и 17

--- Aisha/5 задание/5.4.py
+++ Aisha/5 задание/5.4.py
@@ -1,13 +1,13 @@
-for i in range (1000, 10001):
+for i in range(1000, 10000):
     n = str(i)
     s1 = int(n[0]) + int(n[1])
     s2 = int(n[1]) + int(n[2])
     s3 = int(n[2]) + int(n[3])
 
+    z = max(s1, s2, s3)
+    w = min(s1, s2, s3)
+    s = s1 + s2 + s3 - z - w
 
-    z = str(max(s1, s2, s3))
-    w = str(min(s1, s2, s3))
-
-    res = w + z
+    res = str(s) + str(z)
     if res == '613':
         print(i)
