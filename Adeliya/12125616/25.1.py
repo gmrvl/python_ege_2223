b2ee2c734fc0461f7e7b9de304c36052b3dfb879
gmrvl <4qZfpPyJ8Ub2Van>
2023-01-10 09:47:22 +0300

    G - задание 17, 25

--- Adeliya/12125616/25.1.py
+++ Adeliya/12125616/25.1.py
@@ -1,15 +1,17 @@
 for i in range(110203, 110246):
-    sq = i**0.5
-    if sq!=int(sq):
-        continue
+    sq = i ** 0.5
+    # if sq != int(sq): это используется только когда ищем квадраты
+    #     continue
     sq = int(sq)
-    a=[sq]
-    for dell in range(2,sq):
+    a = []
+    for dell in range(1, sq):
         if i % dell == 0:
-            a.append(dell)
-            a.append(i//dell)
-        if len(a)>4:
-          break
-    if len(a)==4:
-        print()
-
+            if dell % 2 == 0:
+                a.append(dell)
+            if (i // dell) % 2 == 0:
+                a.append(i // dell)
+        if len(a) > 4:
+            break
+    if len(a) == 4:
+        a = sorted(a)
+        print(a)
