afc2553ad9d45b1881d13346664b1b1751cf11f2
gmrvl <4qZfpPyJ8Ub2Van>
2023-01-30 18:28:51 +0300

    g - вариант полякова

--- Adeliya/polyakov.3/16.py
+++ Adeliya/polyakov.3/16.py
@@ -1,11 +1,15 @@
-import sys
+# import sys
+#
+# sys.setrecursionlimit(3600)
+#
+#
+# def F(n):
+#     if n == 1:
+#         return 1
+#     if n > 1:
+#         return (2 * n - 1) * F(n - 1)
+#
+#
+# print(F(3516) / F(3513))
 
-sys.setrecursionlimit(3600)
-
-
-def F(n):
-    if n==1:
-        return 1
-    if n>1:
-        return (2*n - 1)*F(n - 1)
-print(F(3516)/F(3513))
+print((3514*2-1)*(3515*2-1)*(3516*2-1))
