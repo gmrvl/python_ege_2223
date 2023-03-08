701624ea44f4212fde28e73fe32822a1a41f4210
gmrvl <4qZfpPyJ8Ub2Van>
2023-03-08 11:06:56 +0300

    s - ошибки

---
+++ sonya/12602457/24-2.2.py
@@ -0,0 +1,12 @@
+file = open('24.2.txt')
+st = file.readline()
+k = 1
+mx = 0
+for i in range(1, len(st)):
+    if (st[i] == 'K' and st[i-1] == 'L') or (st[i-1] == 'K' and st[i] == 'L'):
+        k = 1
+    else:
+        k += 1
+        if k > mx:
+            mx = k
+print(mx)
