19c766bd5029c7765942a1765b3bf09e7ad49dcc
sonya <sofya.kvastsova@yandex.ru>
2023-05-06 12:20:32 +0300

    -

--- sonya/досрок2023/17.py
+++ sonya/досрок2023/17.py
@@ -1,6 +1,8 @@
 file = open('17.txt')
 min5 = 100000
 for n in file:
+    n = int(n)
+    n = str(n)
     if len(n) == 3:
         if n[-1] == '5':
             min5 = min(min5, int(n))
@@ -12,7 +14,8 @@ for n in file:
     n = int(n)
     if (len(str(n)) == 3 and len(str(last)) != 3) or (len(str(n)) != 3 and len(str(last)) == 3):
         if (n + last) % min5 == 0:
+            print(n, last)
             count += 1
-            maxsumm = max(maxsumm, n**2 + last ** 2)
+            maxsumm = max(maxsumm, n + last)
     last = n
 print(count, maxsumm, min5)
