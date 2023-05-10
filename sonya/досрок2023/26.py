d8f1cfe200c3c162c8fc03eaf7d8a9d9bab2820e
sonya <sofya.kvastsova@yandex.ru>
2023-05-10 23:35:00 +0300

    -

--- sonya/досрок2023/26.py
+++ sonya/досрок2023/26.py
@@ -1,4 +1,25 @@
 file = open('26.txt')
 K = int(file.readline())
 N = int(file.readline())
+a = []
+for i in file:
+    sdacha, vidacha = map(int, i.split())
+    a.append([sdacha, vidacha])
+a.sort()
+count = 0
+maxtime = 0
+maxk = 0
+for i in range(1, K + 1):
+    time = 0
+    for n in a:
+        if n[0] - time >= 1 and n[1] != 10**8 and n[0] != 10**8:
+            count += 1
+            time = n[1]
+            if n[0] >= maxtime:
+                maxtime = n[0]
+                maxk = i
+            n[0] = 10**8
+            n[1] = 10**8
+print(count, maxk)
+
 
