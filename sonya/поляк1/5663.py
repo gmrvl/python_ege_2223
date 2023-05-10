014f97e3b6adf5781d5c5171b7c26dc20f7db329
sonya <sofya.kvastsova@yandex.ru>
2023-05-10 20:30:34 +0300

    -

--- sonya/поляк1/5663.py
+++ sonya/поляк1/5663.py
@@ -1,13 +1,11 @@
 #задание25
-n = []
-for i in range(10068, 17000000):
-    number = str(i)
-    if number.count('1') != 0:
-        if number.find('6') - number.find('1') >= 3 and number.find('8') - number.find('6') == 1:
-            number = int(number)
-            if number % 161 == 0:
-                n.append(number)
-
-print(len(n))
-print(n[0], n[499], n[999])
-print(n[0]//161, n[499]//161, n[999]//161)
+from fnmatch import*
+a = []
+for i in range(1,1000000, 500):
+    a.append(i)
+count = 0
+for i in range(161, 17000001, 161):
+    if fnmatch(str(i), '*1?*?68*'):
+        count += 1
+        if count in a:
+            print(i, i //161, count)
---
+++ sonya/поляк1/5672.py
@@ -0,0 +1,10 @@
+def ДЕЛ(n, m):
+    if n % m == 0:
+        return 1
+    else:
+        return 0
+
+b = []
+for i in range(10,41):
+    b.append(i)
+
