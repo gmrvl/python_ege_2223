2e974952a755ba717e0e20b1f915fbf952142237
sonya <sofya.kvastsova@yandex.ru>
2023-06-10 10:01:20 +0300

    -

---
+++ sonya/якласс/26.py
@@ -0,0 +1,23 @@
+file = open('26.txt')
+K = int(file.readline())
+N = file.readline()
+ns = []
+for n in file:
+    s, v = map(int, n.split())
+    ns.append([s,v])
+ns = sorted(ns)
+count = 0
+maxtime = 0
+nomer = 0
+for i in range(K):
+    time = 0
+    for n in ns:
+        if n[0] - time >= 1 and n[0] != 10**8 and n[1] != 10**8:
+            count += 1
+            time = n[1]
+            if n[0] >= maxtime:
+                maxtime = n[0]
+                nomer = i
+            n[0] = 10**8
+            n[1] = 10**8
+print(count, nomer)
